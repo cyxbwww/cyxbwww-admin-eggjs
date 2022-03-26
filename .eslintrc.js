0c942cfa5aa8b0fc8444b1053ec332137d71d659
cyxbwww <18219326208@163.com>
2022-03-26 14:33:23 +0800

    build: 初始化项目

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'linebreak-style': ['error', 'unix'],
+    semi: ['error', 'never'],
+    quotes: ['error', 'single'],
+  },
+}
